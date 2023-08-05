045e6a16ea8498b3753232fe8684c0fc2cfb2736
jungwonJung <wjdwjd1501@gmail.com>
2023-08-05 14:52:13 +0200

    add 3D animation effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import DisplaySection from "./components/DisplaySection";
 import Jumbotron from "./components/Jumbotron";
+import SoundsSection from "./components/SoundsSection";
+import WebgiViewer from "./components/WebgiViewer";
 import Nav from "./components/nav";
 
 function App() {
@@ -6,6 +9,9 @@ function App() {
     <div className="App">
       <Nav />
       <Jumbotron />
+      <SoundsSection />
+      <DisplaySection />
+      <WebgiViewer />
     </div>
   );
 }
